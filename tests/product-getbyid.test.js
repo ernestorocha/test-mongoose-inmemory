3ab9dc48e9ce0fd9a300d3f7efa6c2494c8a9e76
Ernesto Rocha <Ernesto.RibeiroRocha@sanofi.com>
2022-06-07 00:59:33 +0200

    Removed tests

--- tests/product-getbyid.test.js
+++ tests/product-getbyid.test.js
@@ -6,6 +6,29 @@ const dbHandler = require('./db-handler');
 const productService = require('../src/services/product');
 const productModel = require('../src/models/product');
 
+let productIphoneId;
+
+/**
+ * Seed the database with products.
+ */
+const createProducts = async () => {
+    const createdIphone = await productModel.create(productIphone);
+    productIphoneId = createdIphone.id;
+    await productModel.create(productFitbit);
+};
+
+const productIphone = {
+    name: 'iPhone 11',
+    price: 699,
+    description: 'A new dual‑camera system captures more of what you see and love. '
+};
+
+const productFitbit = {
+    name: 'Fitbit Inspire HR',
+    price: 699,
+    description: 'Get empowered to make a change and embrace your weight and fitness goals... '
+};
+
 /**
  * Connect to a new in-memory database before running any tests.
  */
@@ -42,41 +65,11 @@ describe('product getById ', () => {
      * Should return null if getById doesn't find any product with the provided id.
      */
     it('should return null if nothing is found', async () => {
-        await expect(productService.getById(mongoose.Types.ObjectId()))
-            .resolves
-            .toBeNull();
     });
 
     /**
      * Should return the correct product if getById finds the product with the provided id.
      */
     it('should retrieve correct product if id matches', async () => {
-        const foundProduct = await productService.getById(productIphoneId);
-
-        expect(foundProduct.id).toBe(productIphoneId);
-        expect(foundProduct.name).toBe(productIphone.name);
     });
 });
-
-/**
- * Seed the database with products.
- */
-const createProducts = async () => {
-    const createdIphone = await productModel.create(productIphone);
-    productIphoneId = createdIphone.id;
-    await productModel.create(productFitbit);
-};
-
-let productIphoneId;
-
-const productIphone = {
-    name: 'iPhone 11',
-    price: 699,
-    description: 'A new dual‑camera system captures more of what you see and love. '
-};
-
-const productFitbit = {
-    name: 'Fitbit Inspire HR',
-    price: 699,
-    description: 'Get empowered to make a change and embrace your weight and fitness goals... '
-};
